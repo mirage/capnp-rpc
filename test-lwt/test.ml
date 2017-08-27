e01b2925fa8e48545ec4e1ac3cf67605fdf0d9a6
Thomas Leonard <thomas.leonard@docker.com>
2017-08-27 13:22:43 +0100

    Start with encryption on in the tutorial
    The `Vat_config` API has changed slightly to make this easier.
    
    The `--secret-key-type` option has gone. A vat must always have a key
    now, although it may be ephemeral and is generated lazily. This is
    because even if a vat accepts non-TLS incoming connections, it may still
    need a key for out-bound connections.
    
    Also, turn on `SO_REUSEADDR` for listening sockets so that the server
    can always be restarted.

--- test-lwt/test.ml
+++ test-lwt/test.ml
@@ -243,18 +243,20 @@ let config_result = cmd_result vat_config
 
 let test_options () =
   let term = (Capnp_rpc_unix.Vat_config.cmd, Cmdliner.Term.info "main") in
-  let expected = `Ok (Capnp_rpc_unix.Vat_config.v
-                        ~secret_key:None
+  let config = Cmdliner.Term.eval
+      ~argv:[| "main"; "--secret-key-file=key.pem"; "--listen-address"; "unix:/run/socket" |] term in
+  let expected = `Ok (Capnp_rpc_unix.Vat_config.create
+                        ~secret_key:(`File "key.pem")
                         (`Unix "/run/socket")
                      ) in
-  Cmdliner.Term.eval ~argv:[| "main"; "--secret-key-type=none"; "--listen-address"; "unix:/run/socket" |] term
-  |> Alcotest.check config_result "No key" expected;
-  let expected = `Ok (Capnp_rpc_unix.Vat_config.v
-                       ~secret_key:None
+  Alcotest.check config_result "Unix, same address" expected config;
+  let expected = `Ok (Capnp_rpc_unix.Vat_config.create
+                       ~secret_key:(`File "key.pem")
                        ~public_address:(`TCP ("1.2.3.4", 7001))
                        (`TCP ("0.0.0.0", 7000))
                      ) in
-  Cmdliner.Term.eval ~argv:[| "main"; "--secret-key-type=none";
+  Cmdliner.Term.eval ~argv:[| "main";
+                              "--secret-key-file=key.pem";
                               "--public-address"; "tcp:1.2.3.4:7001";
                               "--listen-address"; "tcp:0.0.0.0:7000" |] term
   |> Alcotest.check config_result "Using TCP" expected
