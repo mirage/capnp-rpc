c9f4a79ddcdde7a84999ef0d3fee75d8aa19f5bc
Thomas Leonard <thomas.leonard@docker.com>
2017-08-29 15:43:27 +0100

    Add Restorer.Table
    This is a simple in-memory table mapping IDs to services.
    
    This is useful if you want to export several interfaces, e.g.
    "admin", "user", etc.

--- capnp-rpc-lwt/capnp_rpc_lwt.mli
+++ capnp-rpc-lwt/capnp_rpc_lwt.mli
@@ -215,6 +215,26 @@ module Restorer : sig
   (** [single id cap] is a restorer that responds to [id] with [cap] and
       rejects everything else. *)
 
+  module Table : sig
+    type t
+    (** A restorer that keeps a hashtable mapping IDs to capabilities in memory. *)
+
+    val create : unit -> t
+    (** [create ()] is a new in-memory table. *)
+
+    val add : t -> Id.t -> 'a Capability.t -> unit
+    (** [add t id cap] adds a mapping to [t].
+        It takes ownership of [cap] (it will call [Capability.dec_ref cap] on [clear]). *)
+
+    val remove : t -> Id.t -> unit
+    (** [remove t id] removes [id] from [t], decrementing the capability's ref count. *)
+
+    val clear : t -> unit
+    (** [clear t] clears the table, dropping all references. *)
+  end
+
+  val of_table : Table.t -> t
+
   val restore : t -> Id.t -> ('a Capability.t, Capnp_rpc.Exception.t) result Lwt.t
   (** [restore t id] restores [id] using [t].
       You don't normally need to call this directly, as the Vat will do it automatically. *)
