1c1d5f87c15efe94e96cf2df41b6b5e4d5b4aa98
Thomas Leonard <thomas.leonard@docker.com>
2017-09-08 10:40:47 +0100

    Add examples/store.mli interface file

---
+++ examples/store.mli
@@ -0,0 +1,35 @@
+(** A store of persistent files.
+    The user can create a new file, get and set its contents, and get a sturdy ref to it.
+    See [test_store] for an example using this. *)
+
+open Capnp_rpc_lwt
+
+module DB : sig
+  type t
+  (** An in-memory data-store. *)
+
+  val create : unit -> t
+  (** [create ()] is a new empty database. *)
+end
+
+module File : sig
+  type t = [`File_aec5916d9557ed0e] Capability.t
+
+  val set : t -> string -> unit Lwt.t
+  (** [set t data] saves [data] as [t]'s contents. *)
+
+  val get : t -> string Lwt.t
+  (** [get t] is the current contents of [t]. *)
+
+  val table : make_sturdy:(Restorer.Id.t -> Uri.t) -> DB.t -> Restorer.Table.t
+  (** [table ~make_sturdy db] is a table of file services, backed by [db].
+      [make_sturdy] is used to generate sturdy URIs for files. *)
+end
+
+type t = [`Store_adb0759941321f8d] Capability.t
+
+val create_file : t -> File.t
+(** [create_file t] is a new file in store [t]. *)
+
+val local : restore:Restorer.t -> DB.t -> t
+(** [local ~restore db] is a file store backed by DB. *)
